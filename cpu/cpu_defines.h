00c03ef36c3c09d4ba8484bf1031d5d8fedc5d7d
foxido <git@forestfox.tk>
2022-10-11 19:01:26 +0300

    UPD: Add missing file

---
+++ cpu/cpu_defines.h
@@ -0,0 +1,69 @@
+#ifndef CLEANUP_DEFINES
+
+#define _STK_UNWRAP(expr)                                                               \
+{                                                                                       \
+    err_flags _stack_res = expr;                                                        \
+    if (_stack_res != res::OK)                                                          \
+    {                                                                                   \
+        if (_stack_res == res::EMPTY)                                                   \
+        {                                                                               \
+            log (log::ERR, "Syntax error: not enough data in stack for operation");     \
+            return CPU_ERRORS::SYNTAX;                                                  \
+        }                                                                               \
+        else                                                                            \
+        {                                                                               \
+            log (log::ERR, "Internal stack errors:");                                   \
+            stack_perror (_stack_res, get_log_stream(), "\t->");                        \
+            return CPU_ERRORS::INTERNAL_ERROR;                                          \
+        }                                                                               \
+    }                                                                                   \
+}
+
+#define CMD_DEF(name, number, code)             \
+case number:                                    \
+    log (log::DBG, "Decoding opcode %2d (%*s)", \
+        number, MAX_OPCODE_LEN, #name);         \
+    {code};                                     \
+    break;
+
+#define OP1   op1
+#define OP2   op2
+#define OPPTR op_ptr
+
+#define POP(arg)  _STK_UNWRAP(stack_pop  (&cpu->stk, arg))
+#define PUSH(arg) _STK_UNWRAP(stack_push (&cpu->stk, arg))
+
+#define GET_ARG()     extract_arg     (cpu, instruct)
+#define GET_ARG_POP() extract_arg_pop (cpu, instruct)
+
+#define JMP(arg) cpu->in = (unsigned int) arg
+
+#define ZERODIV()      return CPU_ERRORS::ZERO_DIV
+#define HLT()          return CPU_ERRORS::OK
+#define SYNTAX_ERROR() return CPU_ERRORS::SYNTAX
+
+#define INP(arg) printf ("Stupid programmer decided to ask you for a number at runtime: \n"); scanf ("%i", &arg)
+#define OUT(arg) printf ("you really can't calculate %i without calc?\n", arg)
+
+#define ERRLOG(fmt, ...) log (log::ERR, fmt, ##__VA_ARGS__)
+
+#else
+
+#undef _STK_UNWRAP
+#undef CMD_DEF
+
+#undef OP1
+#undef OP2
+#undef POP
+#undef PUSH
+#undef GET_ARG
+#undef GET_ARG_POP
+#undef JMP
+#undef ZERODIV
+#undef HLT
+#undef SYNTAX_ERROR
+#undef INP
+#undef OUT
+#undef ERRLOG
+
+#endif
